08841d6349ddee898717b98f7008094694eaba6c
macy <macy.ngan@toptal.com>
2019-10-02 08:21:22 -0700

    add usetrace bot to run test and read result

--- bots/jsonplaceholder.py
+++ bots/jsonplaceholder.py
@@ -1,117 +1,120 @@
-"""
-jsonplaceholder bot
-=============================
-TYPE: EXAMPLE
-API: https://jsonplaceholder.typicode.com
-
-The jsonplaceholder api is uniform enough that we can get away with
-dynamically populating attrs/methods. This is a bit showy for an example,
-but it demonstrates how dynamic a bot can be.
-
-There is only 1 line of botcannon specific code, _entrypoint_ = 'JsonTestBot'
-
-Currently stdout will not be returned to the user,
-only the results of the attribute call chain from fire return to a bot.
-
-"""
 import requests
+import json
+import os
+
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+with open(os.path.join(__location__, "config.json"), "r") as f:
+    config = json.load(f)
 
 
 class JsonTestBot:
-    """
-    --base-url https://jsonplaceholder.typicode.com
-
-    The top of the command tree, all future classes depend on it
-    When used by Botcannon, traversal begins here after satisfying __init__
-
-    This simple class holds the session and some info for our future requests
-
-    The base_url parameter is important
-
-    Botcannon stores these parameters in redis, as defined for each service...
-    ...this makes your bot code reusable for new bots with different api keys on the same host...
-    ...a similar reuse happens with runners, allowing the same bot across many slacks.
-
-    Each attribute is exposed as a command to the bot, but can be hidden from the user by prepending _
-    """
     def __init__(self, base_url):
-        # base_url = 'https://jsonplaceholder.typicode.com'
+        base_url = 'https://team.usetrace.com/rpc/app/init'
         self.r = requests.Session()
-        self.r.headers['Content-type'] = 'application/json'
-        self.endpoints = {
-            'posts': f'{base_url}/posts',
-            'comments': f'{base_url}/comments',
-            'albums': f'{base_url}/albums',
-            'photos': f'{base_url}/photos',
-            'todos': f'{base_url}/todos',
-            'users': f'{base_url}/users',
+        self.results = []
+        self.r.headers={
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-origin',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'User-Agent': config["USER_AGENT"],
+            'Accept': 'application/json, text/javascript, */*; q=0.01',
+            'Referer': 'https://team.usetrace.com/',
+            'X-Requested-With': 'XMLHttpRequest',
+            'Connection': 'keep-alive',
+            'Cookie': config["COOKIES"]
         }
 
-        self.users = Users(self)
+        self.endpoints = {
+            'init': f'{base_url}',
+            'project': 'https://api.usetrace.com/api/project/',
+        }
+
+        self.project = Project(self)
         self._hidden = None
 
 
-class Users:
-    """
-    Uses JsonTestBot to perform tasks with /users
-
-    It's important to be mindful of the calls you make to a REST api as a chatbot.
-    We could, unwisely get all users during __init__ but we don't don't need to for by_id.
-    by_name is bad on purpose, we have could queried the api...
-    ...but it was an opportunity to make a point...don't slow your bots down!!
-    """
+class Project:
     def __init__(self, bot):
         self._b = bot
+        self._projects = self.all()
 
     def all(self):
-        """Get all users by username as a dict"""
-        return {i['username']: i for i in self._b.r.get(self._b.endpoints['users']).json()}
+        """Get all projects"""
+        r = self._b.r.get(self._b.endpoints["init"]).json()
+        data = r["projects"]
+        return {i['id']: i for i in data}
 
-    def by_name(self, username):
-        """Get a User object with username by getting all users"""
-        users = self.all()
-        if username in users:
-            return User(users[username], self._b)
-        else:
-            return 'No user found.'
+    def names(self):
+        projects = self.all()
+        result = []
+        for key in projects:
+            item = projects[key]
+            result.append(item["name"])
+        return "\n".join(result)
 
-    def by_id(self, id):
-        """Get a User object by id"""
-        user_json = self._b.r.get(self._b.endpoints['users'], params=str(id)).json()[0]
-        return User(user_json, self._b)
+    def run(self, arg1, arg2=None, arg3=None, arg4=None):
+        input = arg1
+        if arg2:
+            input = input + " " + arg2
+        
+        if arg3:
+            input = input + " " + arg3
 
+        if arg4:
+            input = input + " " + arg4
 
-class User:
-    """
-    User inherits the keys values pairs from user_json as attributes.
+        projects = self.all()
 
-    Then, using our instance of JsonTestBot (bot) we create methods dynamically
-    from bot.endpoints that pertain to our User. In this case I saved myself from
-    writing 4 additional methods for each category.
+        for key in projects:
+            item = projects[key]
 
-    Look for common ground!
-    In this case the endpoint is the only thing that changes in the request. Ripe for meta.
-    """
-    def __init__(self, user_json, bot):
-        # bot serves as session handling and app structure, the _ hides it from fire/torch
-        self._b = bot
+            if item["name"].lower() == input.lower():
+                payload = {
+                    "requiredCapabilities": [{"browserName": "chrome"}]
+                }
+                url = self._b.endpoints["project"]+ key + '/execute_all'
+                headers = {"Content-type": "application/json"}
+                project_run = requests.post(url, data=json.dumps(payload), headers=headers)
+                return project_run.text
+        
+        return "Invalid project name"
 
-        # assign user_json fields as attrs to this class
-        for attr in user_json:
-            setattr(self, attr, user_json[attr])
+    def result(self, arg1, arg2=None, arg3=None, arg4=None):
+        input = arg1
+        if arg2:
+            input = input + " " + arg2
+        
+        if arg3:
+            input = input + " " + arg3
+        
+        if arg4:
+            input = input + " " + arg4
 
-        # assign methods with names from bot.endpoints to a generated function
-        for endpoint in ["posts", "comments", "comments", "albums", "todos"]:
-            # this is unique per iteration, create a new instance for the function
-            url = self._b.endpoints[endpoint]
+        projects = self.all()
 
-            # generate the function
-            def get():
-                # we are making the assumption that self.id exists from above.
-                return self._b.r.get(url, params={'userId': self.id}).json()
+        for key in projects:
+            item = projects[key]
+            if item["name"] == input:
+                url = self._b.endpoints["project"]+ key + '/lastBatchStatus'
+                project_run = requests.get(url).json()
+                report = []
+                
+                if not project_run["batch"]:
+                    return "no result yet"
+                
+                batch = project_run["batch"]
+                report.append("Id: " + batch["id"])
+                report.append("Requested: " + str(batch["requested"]))
+                report.append("Finished: " + str(batch["finished"]))
+                report.append("Passed: " + str(batch["passed"]))
+                report.append("Failed: " + str(batch["failed"]))
+                return "\n".join(report)
 
-            # set the method
-            setattr(self, endpoint, get)  # < do not call () ! you want the function, not the result of the function
+        return "Invalid project name"
+
 
 
 # _entrypoint_ is the name of the root class botcannon and will load using
